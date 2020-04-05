7d2a91d995f784d3e6fe3ef7bbffe5fea2654d6b
chenlei <745512023@qq.com>
2020-04-05 13:06:45 +0800

    feat: complete Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button className="button1" size={ButtonSize.Small} autoFocus>
+          按钮1
+        </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          hello
+        </Button>
+        <Button btnType={ButtonType.Danger}>hello</Button>
+        <Button
+          btnType={ButtonType.Link}
+          href="http://www.baidu.com"
+          target="_blank"
+        >
+          link to url
+        </Button>
+        <Button
+          btnType={ButtonType.Link}
+          disabled={true}
+          href="http://www.baidu.com"
+        >
+          <span>link to url</span>
+        </Button>
+        <Button
+          btnType={ButtonType.Primary}
+          onClick={() => window.alert("nnn")}
+          target="_blank"
+        >
+          按钮3
+        </Button>
+        <span>321</span>
+        <h1>Hello World</h1>
+        <h2>Hello World</h2>
+        <h3>Hello World</h3>
+        <hr />
+        <code>const a = 321;</code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
