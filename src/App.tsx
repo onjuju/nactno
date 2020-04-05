39a9ad5b55e9d9675f924ad86013881da92a0f06
chenlei <745512023@qq.com>
2020-04-05 14:43:52 +0800

    feat: add Alter Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+import Alert from "./components/Alert";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button className="button1" size={ButtonSize.Small} autoFocus>
+        {/* <Button className="button1" size={ButtonSize.Small} autoFocus>
           按钮1
         </Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
@@ -32,24 +33,16 @@ function App() {
           target="_blank"
         >
           按钮3
-        </Button>
-        <span>321</span>
-        <h1>Hello World</h1>
-        <h2>Hello World</h2>
-        <h3>Hello World</h3>
-        <hr />
-        <code>const a = 321;</code>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </Button> */}
+        <Alert type="default" content="default content" />
+        <Alert type="success" content="success content" title="提示标题" />
+        <Alert type="danger" content="danger content" title="提示标题" />
+        <Alert
+          type="warning"
+          content="warning content"
+          title="提示标题"
+          closable
+        />
       </header>
     </div>
   );
